20d083323b4a13ced6560800c27d318b44fa66c2
nebeltag <nebeltag@mail.ru>
2023-03-23 19:38:45 +0300

    homework_2(complete)

--- homework_2/test.php
+++ homework_2/test.php
@@ -5,17 +5,28 @@ $answer2 = '10';
 $answer3 = '5+5';
 $bingo = '10';
 
+// do {
+//   $userInput = 
+//   (string)readline("{$question} {$answer1}, {$answer2}, {$answer3}\n ");
+//   if ($userInput == $bingo){
+//     echo "Поздравляем! Ваш ответ верный!\n";
+//       break;} 
+//   if ($userInput == $answer1 || $userInput == $answer2 || $userInput == $answer3) {
+//     echo "Ответ неверный!\n";
+//       break;} 
+// }
+// while($userInput != $bingo); 
+
+
 do {
   $userInput = 
   (string)readline("{$question} {$answer1}, {$answer2}, {$answer3}\n ");
-  if ($userInput == $bingo){
-    echo "Поздравляем! Ваш ответ верный!\n";
-      break;} 
-  if ($userInput == $answer1 || $userInput == $answer2 || $userInput == $answer3) {
-    echo "Ответ неверный!\n";
-      break;} 
-}
-while($userInput != $bingo); 
+} while($userInput != $answer1 && $userInput != $answer2 && $userInput != $answer3); 
 
+  if ($userInput == $bingo) {
+    echo "Поздравляем! Ваш ответ верный!\n";
+  } else {
+    echo "Ответ неверный!\n";
+  } 
 
 
